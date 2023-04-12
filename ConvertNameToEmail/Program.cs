f7c52640c27f1d4414baa6d8e64b0062fe6a6a35
stasko06 <shketievstanislav@gmail.com>
2023-04-12 22:32:52 +0300

    Add project files.

---
+++ ConvertNameToEmail/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+static bool CheckNameLenght(string name)
+{
+    if (name.Length>20 || name.Length == 0)
+    {
+        Console.WriteLine("The name can't hold more than 20 characters or be empty!");
+        return false;
+    }
+    else return true;
+    
+}
+string name = Console.ReadLine();
+bool check = CheckNameLenght(name);
+while (check == false)
+{
+    name  = Console.ReadLine();
+    check =  CheckNameLenght(name);
+}
+if (check == true)
+{
+    name = name.ToLower();
+    name = name.Trim();
+    name = name.Replace(" ", "");
+    Console.WriteLine("Email:");
+    name = name + "@mail.com";
+    Console.WriteLine(name);
+}
+
